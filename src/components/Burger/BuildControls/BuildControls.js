77523ac01b28d1c5a837b77d6bf715b75ea89b2d
Alex Reckard <alexreckard@gmail.com>
2019-02-17 23:01:29 -0700

    Grammar changes

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -24,7 +24,7 @@ const buildControls = (props) => (
         ))}
         <button
           className={classes.OrderButton}
-          disabled={!props.purchasabale}
+          disabled={!props.purchasable}
           onClick={props.ordered}>ORDER NOW</button>
     </div>
 );
